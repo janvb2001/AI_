5e6227b0fb060004e8999dc60dd281f53502bd95
Jan van Buuren <49169548+janvb2001@users.noreply.github.com>
2024-10-15 12:40:07 +0200

    Update dataset.py

--- competition/dataset.py
+++ competition/dataset.py
@@ -1,8 +1,11 @@
 import torch
+import torchvision
 from torch.utils.data import Dataset
 import numpy as np
 import h5py
+import hdf5plugin
 import os
+import torch.nn.functional as F
 
 
 class droneDataSet(Dataset):
@@ -27,9 +30,21 @@ class droneDataSet(Dataset):
     def __getitem__(self, idx):
         if self.h5:
             images, targets = (
-                torch.tensor(self.images[idx]),
-                torch.tensor(self.targets[idx]),
+                torchvision.transforms.ToTensor()(self.images[idx]),
+                # torch.tensor(np.array(self.images[idx]).Totensor()),
+                torch.tensor(np.array(self.targets[str(idx).zfill(5)])),
             )
+
+            images = torch.moveaxis(images, (1, 2), (0, 1))
+
+            targets = targets.flatten()
+            padding_length = 108 - targets.size(0)
+
+            if padding_length > 0:
+                targets = F.pad(targets, (0, padding_length), "constant", 0)
+            else:
+                targets = targets[:109]
+
         else:
             images, targets = self.images[idx], self.targets[idx]
 
@@ -38,3 +53,4 @@ class droneDataSet(Dataset):
             None
 
         return images, targets
+
